f729af04f83279a13d58359c2f5ab823671a6397
Awn <awnawnawnawnawn@gmail.com>
2016-11-13 01:20:00 +0900

    板トップ画面での処理ロジックを変更。

--- js/itatop.js
+++ js/itatop.js
@@ -21,24 +21,27 @@
 //-20xxxxxx(ver 1.0.0) : 新規作成
 //
 ////////////////////////////////////////////////////////////////////////////////
-
-
-//板トップで動くスクリプト
-
 (function() {
-
     //member
     var _info = {};
+    var _sures;
+    var _topThreads;
+    var _suretaiaboned;
+
 
     //init
     _init();
 
+
     //method
     function _init() {
         _info = _getPageInfo();
+        _topThreads = document.getElementById("topThreads").childNodes[1];
+        _sures = _trimThreads();
         _assignEventHandler();
         return;
     }
+
 
     function _getPageInfo() {
         var _url = location.href;
@@ -52,60 +55,91 @@
         }
     }
 
+
     function _assignEventHandler() {
         chrome.runtime.onMessage.addListener(function(parm, sender, sendResponse) {
-            if (parm.suretaikaigyou === "yes") {
-                _doSuretaiKaigyou();
-            }
+
             if (parm.suretaiabone === "yes") {
-                _doSuretaiAbone(parm.ngsuretai);
+                _suretaiaboned = _doSuretaiAbone(parm.ngsuretai);
+            } else {
+                _suretaiaboned = _sures;
+            }
+            if (parm.suretaiabone === "yes" || parm.suretaikaigyou === "yes") {
+                _replaceTopThreads(parm.suretaikaigyou);
             }
         });
         return;
     }
 
 
-    function _doSuretaiKaigyou() {
-        //変数
-        var surekey;
-        var date;
+    function _replaceTopThreads(kaigyou) {
+        var _tail = kaigyou === "yes" ? "<br>" : "　";
+        var str;
+        var output = _suretaiaboned.map(function(elm) {
+            str = "<a href='" + elm.url + "'target='body'><t>" + elm.suretai + "</t></a>";
+            //str += "<span class='ank'><a rel='nofollow' href='" + elm.url.slice(0,-3) + "1' class='anked'>[#]</a>";
+            str += _tail;
+            return str;
+        });
 
-        //#topThreadsを取得
-        var _d = document.getElementById("topThreads").childNodes[1].children;
-        var _dlen = _d.length;
-
-        _d[0].outerHTML = "　" + _d[0].outerHTML;
-
-        for (var ix = 0; ix < _dlen; ix++) {
-            if (ix < 18 && ix % 2 === 0) {
-                //surekey は _d[num] s.t. num % 2 === 0に入っている
-                surekey = _d[ix].getAttribute("href").split("/")[4] - 0;
-                date = _getSuretateDate(surekey);
-                _d[ix].innerText = "【" + date + "】" + _d[ix].innerText;
-
-                //改行したいのは _d[num] s.t. num % 2 === 1
-                _d[ix + 1].innerHTML += "<br>"; //スレタイ
-
-            } else if (ix >= 18) {
-                surekey = _d[ix].getAttribute("href").split("/")[4] - 0;
-                date = _getSuretateDate(surekey);
-                _d[ix].innerText = "【" + date + "】" + _d[ix].innerText
-                _d[ix].innerHTML += "<br>"; //スレタイ
-            }
-        }
+        //_topThreads.innerHTMLを入れ替える。
+        _topThreads.innerHTML = output.join("");
         return;
     }
 
+
+    function _trimThreads() {
+        var d = _topThreads.children;
+        var output = [];
+
+        for (var ix = 0, len = d.length; ix < len; ix++) {
+            if (ix < 18 && ix % 2 === 0) {
+                output.push({
+                    suretai: ((ix / 2) + 1 + "") + ":" + d[ix + 1].innerText,
+                    url: d[ix].getAttribute("href")
+                });
+            } else if (ix >= 18) {
+                output.push({
+                    suretai: d[ix].innerText,
+                    url: d[ix].getAttribute("href")
+                });
+            }
+        }
+        return output;
+    }
+
+
     function _getSuretateDate(input) {
+        //surekeyをスレ立て日時に変換する
         var timestamp = input * 1000;
         var p = new Date(timestamp);
         var rtn = p.getFullYear() + "/" + ("0" + (p.getMonth() + 1)).substr(-2, 2) + "/" + ("0" + p.getDate()).substr(-2, 2) + " " + ("0" + p.getHours()).substr(-2, 2) + ":" + ("0" + p.getMinutes()).substr(-2, 2) + ":" + ("0" + p.getSeconds()).substr(-2, 2);
         return rtn;
     }
 
-    function _doSuretaiAbone(nglist){
-    	console.log(nglist);
-    	return;
+
+    function _doSuretaiAbone(list) {
+        //改行で分割 -> 配列
+        nglist = list.split("\n");
+
+        //空白除去
+        nglist = nglist.filter(function(elm) {
+            return elm !== "";
+        });
+
+        //正規表現化
+        var ngregs = nglist.map(function(elm) {
+            return new RegExp(elm);
+        });
+
+        //正規表現を適用する
+        var output = _sures.filter(function(elm) {
+            return !ngregs.some(function(inelm) {
+                return inelm.test(elm.suretai);
+            });
+        });
+
+        return output;
     }
 
     return _info;
