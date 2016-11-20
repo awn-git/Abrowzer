2c9a1c5dfb73b2afef5221283b9ad7b59148260a
Awn <awnawnawnawnawn@gmail.com>
2016-11-20 22:26:45 +0900

    add class and html element in parsed dat file. some change in css file

--- js/dat.js
+++ js/dat.js
@@ -10,71 +10,66 @@
 //.AMA.   .AMMA.P^YbmdP'.JMML.   `Ybmd9'     W      W     AMMmmmM `Mbmmd'.JMML.  
 ////////////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////////////////////
-//
-//@title dat.js
-//@description 一行で説明を書く
-//
-//作った人: Awn(@Awn_tw)
-//
-//改定履歴
-//-20xxxxxx(ver 1.0.0) : 新規作成
-//
-////////////////////////////////////////////////////////////////////////////////
-
 (function() {
     var d = document;
     var pre = d.getElementsByTagName("pre")[0];
     var arr = pre.innerText.split("\n");
+
+    //preタグ内の最後に存在する改行のみの行を取り除く
     arr.pop();
 
     var suretai = arr[0].match(/ <>(.*)$/)[1];
     d.title = suretai;
     arr[0] = arr[0].replace(/ <>(.*)$/, "");
-    arr[0] = arr[0].replace(/<> (.*)$/, "<div class='resbody'>$&</div>");
 
     var lh = location.href.match(/(^.*open2ch.net\/)(.*)\/dat\/([0-9]{10})/);
     var host = lh[1];
     var bbsname = lh[2];
     var surekey = lh[3];
+    var gazou = host + "test/image.cgi/" + bbsname + "/" + surekey + "/";
     var itatop = host + bbsname;
     var subback = itatop + "/" + "subback.html";
 
     var header = "";
-    header += "<a href='" + itatop + "'>■板に戻る</a> | ";
+    header += "<a href='http://menu.open2ch.net/bbsmenu.html'>★BBSMENUに戻る</a><br>";
+    header += "<a href='" + itatop + "/'>■板に戻る</a> | ";
     header += "<a href='http://open2ch.net/test/history.cgi'>履歴に戻る</a> | ";
     header += "<a href='" + subback + "'>★スレ一覧</a> | ";
+    header += "<a href='" + gazou + "'>★画像一覧</a> | ";
     header += "<a href='#bottom'>↓</a><a name='top'></a>";
     header += "<h3 style='color:red;'>" + suretai + "</h3>";
     header += "<hr>";
 
-    var str;
-    var resnum;
-    var arrm = arr.map(function(elm, ind) {
-        elm = elm.replace(/<> (.*)<>$/, "<br><div class='resbody'>$&</div>")
-            .replace(/<\/b>/g, "")
-            .replace(/<b>/g, "")
-            .replace(/^(.*?)<+?>+?/, "<b style='color:green'>$&</b> ")
-            .replace(/<>/g, " ")
-            .replace(/https?:\/\/[a-zA-Z0-9-_.:@!~*';\/?&=+$,%#]+/g,
-                "<a href='$&' target='_blank'>$&</a>")
-            .replace(/&gt;&gt;[0-9]{1,3}/g, "<a href='#$&'>$&</a>");
-
-        resnum = "<a name='>>" + (ind + 1) + "'>" + (ind + 1) + "</a>" + " :";
-        elm = resnum + " " + elm;
-
-        str = "<div>" + elm + "</div>";
-        return str;
-    });
-
     var footer = "";
     footer += "<hr>";
-    footer += "<div style='text-align:center'><a href='#bottom ' id='newres'>新着レスの表示</a> | <a href='#bottom' id='kokomade'>ここまで読んだ？</a></div>";
+    footer += "<div style='text-align:center'><a href='" + location.href + "' id='newres'>新着レスの表示</a> | <a href='#bottom' id='kokomade'>ここまで読んだ？</a></div>";
     footer += "<hr>";
-    footer += "<a href='" + itatop + "'>■板に戻る</a> | ";
+    footer += "<a href='http://menu.open2ch.net/bbsmenu.html'>★BBSMENUに戻る</a><br>";
+    footer += "<a href='" + itatop + "/'>■板に戻る</a> | ";
     footer += "<a href='http://open2ch.net/test/history.cgi'>履歴に戻る</a> | ";
     footer += "<a href='" + subback + "'>★スレ一覧</a> | ";
+    footer += "<a href='" + gazou + "'>★画像一覧</a> | ";
     footer += "<a href='#top'>↑</a><a name='bottom'></a>";
+
+    var str;
+    var resnum;
+    var arrmap = arr.map(function(elm, ind) {
+        elm = elm.replace(/<\/b>/g, "")
+            .replace(/<b>/g, "")
+            .replace(/^(.*?)<+?>+?/, "<b style='color:green' class='name'>$&</b> ")
+            .replace(/ID:(.*?)<+?>+? /, "<span class='id'>$1</span>")
+            .replace(/<\/span>(.*$)/, "<\/span><div class='resbody'>$1</div>")
+            .replace(/<>/g, "")
+            .replace(/https?:\/\/[a-zA-Z0-9-_.:@!~*';\/?&=+$,%#]+/g,
+                "<a href='$&' target='_blank'>$&</a>")
+            .replace(/&gt;&gt;([0-9]{1,3})/g, "<a href='#$1'>$&</a>");
+
+        resnum = "<a name='" + (ind + 1) + "'>" + (ind + 1) + "</a>" + " :";
+        elm = resnum + " " + elm;
+
+        str = "<div class='res'>" + elm + "</div>";
+        return str;
+    });
 
     var phorm = "";
     phorm += "<form method='POST' action='/test/bbs.cgi?guid=ON' id='form2' style='margin-top:5pt'>";
@@ -83,11 +78,17 @@
     phorm += "mail: " + "<input size='10' id='mail' name='mail'></input><br>";
     phorm += "<textarea rows='5' cols='56' id='MESSAGE' name='MESSAGE'></textarea>";
     phorm += "<input type='hidden' name='bbs' value='" + bbsname + "'>";
-    phorm += "<input type='hidden' name='key' value='"  + surekey  + "'>";
-    phorm += "<input type='hidden' name='time' value='" + Math.floor( Date.now()/1000 ) + "'>";
+    phorm += "<input type='hidden' name='key' value='" + surekey + "'>";
+    phorm += "<input type='hidden' name='time' value='" + Math.floor(Date.now() / 1000) + "'>";
     phorm += "</form>";
 
-    d.body.innerHTML = header + arrm.join("") + footer + phorm;
+    var bodyinner = "<div class='header'>" + header + "</div>";
+    bodyinner += "<div class=thread>" + arrmap.join("") + "</div>";
+    bodyinner += "<div class='footer'>" + footer + "</div>";
+    bodyinner += "<div class='form'>" + phorm + "</div>";
+
+    d.body.innerHTML = bodyinner;
+    window.scrollTo(0, 0);
 
     kokomade();
     newres();
